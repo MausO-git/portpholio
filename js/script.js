ab57b1a4b8bb829b4ac846f607ad2e8f30b7a6be
MausO <oscarmaus01@gmail.com>
2025-08-24 19:35:03 +0200

    pres test

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const contactButton = document.querySelector('.menu .contactButton');
 const contactZone = document.querySelector('#contact');
 const arrow = document.querySelector('#contact .arrow');
 const card = document.querySelector('.fonc');
+const nom = document.querySelector('.nom');
+const gnom = document.querySelector('.groupName');
 
 
 const seeForm = ()=>{
@@ -37,3 +39,33 @@ document.addEventListener("mouseleave", () => {
     card.style.transform = "rotateX(0) rotateY(0)";
     card.style.boxShadow = "none";
 });
+
+nom.addEventListener('click', ()=>{
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+        <p>
+            \\\\Bonjour, je suis Oscar Maus, jeune développeur web motivé à concevoir des sites clairs, efficaces et sécurisés. Passionné par le code, je m’appuie sur différentes technologies du web et reste toujours curieux d’en apprendre de nouvelles afin d’élargir mes compétences et relever de nouveaux défis.
+        </p>
+        <div class='cross'>
+            <img src='images/pxcross.png' >
+        </div>
+    `;
+    newDiv.classList.add('pres');
+    gnom.appendChild(newDiv);
+    const pres = document.querySelector('.pres');
+    setTimeout(()=>{
+        pres.style.transform = 'translate(-50%, -35%) scale(1)'
+    }, 300)
+    const cross = document.querySelector('.cross')
+    cross.addEventListener('click', ()=>{
+        pres.style.transform = 'translate(-50%, -35%) scale(0)'
+        setTimeout(()=>{
+            gnom.removeChild(pres);
+        }, 300)
+    })
+    
+
+})
+
+
+
